58ad121031d9fd2383a5ab86abe2eeadee1c3b2c
rahul <rahul@DESKTOP-A4ORL8F>
2024-01-15 19:55:54 +0530

    Add project files.

---
+++ Employee/Controllers/EmployeePortfoliosController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Employee.Models;
+
+namespace Employee.Controllers
+{
+    public class EmployeePortfoliosController : Controller
+    {
+        private EmployeePortfolioContext db = new EmployeePortfolioContext();
+
+        // GET: EmployeePortfolios
+        //public ActionResult Index()
+        //{
+        //    return View(db.EmployeePortfolios.ToList());
+        //}
+
+        public ActionResult Index(string email)
+        {
+            if (email != "" && email!=null)
+            {
+                EmployeePortfolio employeePortfolio = db.EmployeePortfolios.FirstOrDefault(x => x.Email == email);
+                if (employeePortfolio != null)
+                    return View(employeePortfolio);
+                else
+                    return View("Create");
+            }
+            else
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        }
+        // GET: EmployeePortfolios/Details/5
+        //public ActionResult Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    EmployeePortfolio employeePortfolio = db.EmployeePortfolios.Find(id);
+        //    if (employeePortfolio == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(employeePortfolio);
+        //}
+
+        // GET: EmployeePortfolios/Create
+        public ActionResult Create()
+        {
+            var allSkills = db.Skills.Select(s => new SelectListItem
+            {
+                Value = s.Id.ToString(),
+                Text = s.Name
+            }).ToList();
+            ViewBag.Skills = new SelectList(allSkills, "Value", "Text");
+            return View();
+        }
+
+        // POST: EmployeePortfolios/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Projects,Education,Achievements,References,Email")] EmployeePortfolio employeePortfolio)
+        {
+            if (ModelState.IsValid)
+            {
+                db.EmployeePortfolios.Add(employeePortfolio);
+                db.SaveChanges();
+                return RedirectToAction("Index",new {email=employeePortfolio.Email });
+            }
+            var Skills = db.Skills.ToList();
+            ViewBag.Skills = new MultiSelectList(Skills, "Id", "Name");
+
+            return View(employeePortfolio);
+        }
+
+        // GET: EmployeePortfolios/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EmployeePortfolio employeePortfolio = db.EmployeePortfolios.Find(id);
+            if (employeePortfolio == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employeePortfolio);
+        }
+
+        // POST: EmployeePortfolios/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,PersonalInformation,Education,Achievements,References")] EmployeePortfolio employeePortfolio)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employeePortfolio).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(employeePortfolio);
+        }
+
+        // GET: EmployeePortfolios/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EmployeePortfolio employeePortfolio = db.EmployeePortfolios.Find(id);
+            if (employeePortfolio == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employeePortfolio);
+        }
+
+        // POST: EmployeePortfolios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            EmployeePortfolio employeePortfolio = db.EmployeePortfolios.Find(id);
+            db.EmployeePortfolios.Remove(employeePortfolio);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Employee/Models/EmployeePortfolio.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Web.Mvc;
+
+namespace Employee.Models
+{
+    public class EmployeePortfolio
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public  string Projects { get; set; }
+        public virtual ICollection<Skill> Skills { get; set; }
+        public string gender { get; set; }
+        public string Education { get; set; }
+        public string Achievements { get; set; }
+        public string References { get; set; }
+    }
+    public class Skill
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+    }
+    public class EmployeeViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Projects { get; set; }
+        public string gender { get; set; }
+        public string Education { get; set; }
+        public string Achievements { get; set; }
+        public string References { get; set; }
+        public IEnumerable<int> SelectedSkillIds { get; set; }
+        public IEnumerable<SelectListItem> AvailableSkills { get; set; }
+
+    }
+    public class EmployeePortfolioContext : DbContext
+    {
+        public DbSet<EmployeePortfolio> EmployeePortfolios { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+    }
+}
